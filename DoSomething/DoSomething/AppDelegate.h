da1984e5948ac192728579d4b5cc12e31f71dde8
Keyar Srinivasan <keyar@oclocksolutions.com>
2015-10-09 12:49:27 +0530

    Initial Source code

---
+++ DoSomething/DoSomething/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoSomething
+//
+//  Created by Keyar on 09/10/15.
+//  Copyright © 2015 OClock Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
