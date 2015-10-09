21337a38b2461185959a2192d322c157892b70ef
OCSDEV2 <ocsdev2@Nandhakumar-iMac.local>
2015-10-09 15:33:03 +0530

    Created Login View

---
+++ DoSomething/DoSomething/ViewController/Login/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  DoSomething
+//
+//  Created by OCSDEV2 on 09/10/15.
+//  Copyright (c) 2015 OClock Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ DoSomething/DoSomething/ViewController/Login/LoginViewController.m
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.m
+//  DoSomething
+//
+//  Created by OCSDEV2 on 09/10/15.
+//  Copyright (c) 2015 OClock Apps. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+
+@end
