d835d9a9c00f1f5f1e8ce6a5ff9321a4b9a2c614
OCSDEV2 <ocsdev2@Nandhakumar-iMac.local>
2015-10-09 17:18:31 +0530

    set up the constraints for login view

--- DoSomething/DoSomething/ViewController/Login/DSLoginViewController.m
+++ DoSomething/DoSomething/ViewController/Login/DSLoginViewController.m
@@ -13,10 +13,32 @@
 @end
 
 @implementation DSLoginViewController
-
+@synthesize temp,facebookLabel,emailLabel,buttonActionLabel,termsOfUseLabel,privacyPolicyLabel,signinLabel;
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    if ([temp isEqualToString:@"createAnAccount"]){
+        facebookLabel.text =@"Create an account using Facebook";
+        emailLabel.text =@"or sign up with your email";
+        buttonActionLabel.text =@"Create Your Account";
+        termsOfUseLabel.text =@"Terms of Use";
+        privacyPolicyLabel.text =@"Privacy Policy";
+        signinLabel.text =@"Have an account? Sign In";
+
+
+            }
+    if ([temp isEqualToString:@"Signin"]){
+        facebookLabel.text =@"Log in with Facebook";
+        emailLabel.text =@"or log in with your email";
+        buttonActionLabel.text =@"Sign in";
+        
+    }
+
+    
+    
+    UIView *statusBarView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 20)];
+    statusBarView.backgroundColor = [UIColor redColor];
+    [self.navigationController.navigationBar addSubview:statusBarView];    // Do any additional setup after loading the view from its nib.
 }
 
 - (void)didReceiveMemoryWarning {
