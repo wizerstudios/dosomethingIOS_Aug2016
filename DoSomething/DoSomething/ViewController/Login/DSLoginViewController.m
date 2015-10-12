baf69b4f13e5b34d2cdd31d6679827e144e0bb93
Sha <ocsdev4@Y8783T53YM.local>
2015-10-12 17:04:37 +0530

    -Modified the constraints

--- DoSomething/DoSomething/ViewController/Login/DSLoginViewController.m
+++ DoSomething/DoSomething/ViewController/Login/DSLoginViewController.m
@@ -8,8 +8,8 @@
 
 #import "DSLoginViewController.h"
 #import "DSSplashViewController.h"
-#define PATRON_BOLD(Value)      [UIFont fontWithName:@"Patron-Bold" size:Value]
-#define PATRON_REG(Value)       [UIFont fontWithName:@"Patron-Regular" size:Value]
+#import "DSConfig.h"
+
 
 
 @interface DSLoginViewController ()
@@ -17,46 +17,43 @@
 @end
 
 @implementation DSLoginViewController
-@synthesize temp,facebookLabel,emailLabel,buttonActionLabel,termsOfUseButton,privacyPolicyButton,signInButton,forgotButton,createAnAccButton,instructionLabel,createAnAccLabel;
+@synthesize temp,labelFacebook,labelEmail,labelSignIn,buttonTermsOfUse,buttonPrivacyPolicy,buttonSignIn,buttonForgotPass,buttonCreateAnAcc,labelInstruction,labelCreateAnAcc;
 - (void)viewDidLoad {
     [super viewDidLoad];   
     [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
-
     
-    if ([temp isEqualToString:@"createAnAccount"]){
-        self.signInButtonHeight.constant =-30;
-        NSString *string = @"Create an account using Facebook";
-        NSMutableAttributedString *attStr = [[NSMutableAttributedString alloc] initWithString:string ];
-        [attStr addAttribute:NSFontAttributeName value:PATRON_REG(12) range:[string rangeOfString:@"Create an account using"]];
-        
-        [attStr addAttribute:NSFontAttributeName value:PATRON_BOLD(12) range:[string rangeOfString:@"Facebook"]];
-        facebookLabel.attributedText = attStr;
-        
-        emailLabel.text =@"or sign up with your email";
-        createAnAccLabel.text =@"Create Your Account";
-        instructionLabel.text =@"By selecting this,you agree to our Terms of Use and our Privacy Policy";
-        createAnAccButton.hidden =YES;
-        forgotButton.hidden=YES;
+ if ([temp isEqualToString:@"createAnAccount"]){
+      self.buttonSignInHeightConstraint.constant =-30;
+      NSString *string = @"Create an account using Facebook";
+      NSMutableAttributedString *attStr = [[NSMutableAttributedString alloc] initWithString:string ];
+      [attStr addAttribute:NSFontAttributeName value:PATRON_REG(12) range:[string rangeOfString:@"Create an account using"]];
+      [attStr addAttribute:NSFontAttributeName value:PATRON_BOLD(12) range:[string rangeOfString:@"Facebook"]];
+      labelFacebook.attributedText = attStr;
+      labelEmail.text =@"or sign up with your email";
+      labelCreateAnAcc.text =@"Create Your Account";
+      labelInstruction.text =@"By selecting this,you agree to our Terms of Use and our Privacy Policy";
+      [buttonTermsOfUse setTitle:@"Terms of Use" forState:UIControlStateNormal];
+      [buttonPrivacyPolicy setTitle:@"Privacy Policy" forState:UIControlStateNormal];
+      [buttonSignIn setTitle:@"Have an account? Sign In" forState:UIControlStateNormal];
+      buttonCreateAnAcc.hidden =YES;
+      buttonForgotPass.hidden=YES;
 
-
-            }
-    if ([temp isEqualToString:@"Signin"]){
-
+}
+ if ([temp isEqualToString:@"Signin"]){
+       
+      NSString *string = @"Log in with Facebook";
+      NSMutableAttributedString *attStr = [[NSMutableAttributedString alloc] initWithString:string ];
+     [attStr addAttribute:NSFontAttributeName value:PATRON_REG(12) range:[string rangeOfString:@"Log in with"]];
+     [attStr addAttribute:NSFontAttributeName value:PATRON_BOLD(12) range:[string rangeOfString:@"Facebook"]];
+     labelFacebook.attributedText = attStr;
         
-            NSString *string = @"Log in with Facebook";
-            
-            NSMutableAttributedString *attStr = [[NSMutableAttributedString alloc] initWithString:string ];
-            [attStr addAttribute:NSFontAttributeName value:PATRON_REG(12) range:[string rangeOfString:@"Log in with"]];
-        
-            [attStr addAttribute:NSFontAttributeName value:PATRON_BOLD(12) range:[string rangeOfString:@"Facebook"]];
-            facebookLabel.attributedText = attStr;
-        
-        emailLabel.text =@"or log in with your email";
-        buttonActionLabel.text =@"Sign in";
-        privacyPolicyButton.hidden =YES;
-        termsOfUseButton.hidden =YES;
-        signInButton.hidden =YES;
-        
+     labelEmail.text =@"or log in with your email";
+     labelSignIn.text =@"Sign in";
+     [buttonForgotPass setTitle:@"Forgot password?" forState:UIControlStateNormal];
+     [buttonCreateAnAcc setTitle:@"Create an Account" forState:UIControlStateNormal];
+     buttonPrivacyPolicy.hidden =YES;
+     buttonTermsOfUse.hidden =YES;
+     buttonSignIn.hidden =YES;
     }
 
     
@@ -79,8 +76,7 @@
 
 
 - (IBAction)Back:(id)sender {
-    DSSplashViewController * DSSplashView  = [[DSSplashViewController alloc]initWithNibName:@"DSSplashViewController" bundle:nil];
-    [self.navigationController pushViewController:DSSplashView animated:YES];
+    [self.navigationController popViewControllerAnimated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
