055f8facc9645c5c1d8fcabb8766d7e38f42297e
Sha <ocsdev4@y8783t53ym.local>
2016-01-08 11:21:07 +0530

    Fixed  Slider:18  font alignment issue

--- DoSomething/DoSomething/ViewController/Location/NearByDetail/DSNearByDetailViewController.m
+++ DoSomething/DoSomething/ViewController/Location/NearByDetail/DSNearByDetailViewController.m
@@ -296,7 +296,7 @@
     if ( indexPath.row ==1)
     {
         NSString *objstr=[userDetailsArray valueForKey:@"about"];
-         dataSize = [COMMON getControlHeight:objstr withFontName:@"Patron-Medium" ofSize:10.0 withSize:CGSizeMake(tableView.frame.size.width,tableView.frame.size.height)];
+         dataSize = [COMMON getControlHeight:objstr withFontName:@"Patron-Medium" ofSize:10.0 withSize:CGSizeMake(150,tableView.frame.size.height+60)];
         if([objstr isEqualToString:@""]|| dataSize.height <=24)
         {
             return 30 ;
@@ -305,10 +305,10 @@
         
         else
         {
-            self.aboutTextHeight.constant=dataSize.height+45;
+            self.aboutTextHeight.constant=dataSize.height+50;
             self.aboutviewHeight.constant=self.aboutTextHeight.constant;
             
-            return self.aboutviewHeight.constant;
+            return self.aboutviewHeight.constant-10;
        }
         
     }
