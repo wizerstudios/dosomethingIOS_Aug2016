2b48ed4dea361980810eb3e733a95621a1a8d507
ChitraMohan <chitra.m@oclocksoftware.com>
2016-02-02 14:03:01 +0530

    APNS for iOS 7

--- DoSomething/DoSomething/Common/AppDelegate/AppDelegate.m
+++ DoSomething/DoSomething/Common/AppDelegate/AppDelegate.m
@@ -245,17 +245,14 @@
 - (void)initAPNS{
     if(IS_GREATER_IOS8)
     {
-        if ([[UIApplication sharedApplication] respondsToSelector:@selector(registerForRemoteNotifications)]) {
 
-                 [[UIApplication sharedApplication] registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert) categories:nil]];
-                 [[UIApplication sharedApplication] registerForRemoteNotifications];
-            
-            
-        } else {
-            
-                [[UIApplication sharedApplication] registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound];
-
-        }
+        [[UIApplication sharedApplication] registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert) categories:nil]];
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+        
+        
+    }
+    else{
+        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound];
     }
     
 }
